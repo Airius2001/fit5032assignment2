e7b3a395de80c96dcf21231a2d6b9464984a029e
Zexin <zxuu0075@student.monash.edu>
2024-09-01 15:26:25 +1000

    deploy project

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fit5032assignment2/",
   plugins: [
     vue(),
     vueDevTools(),
